ba3428169fdfff0c4c549da14bfad143fd3a8c03
Nicola <nyc.zyp@gmail.com>
2025-09-05 23:12:55 +0100

    5.54 - Writing linker script from scratch part 3

--- src/startup_stm32f303.rs
+++ src/startup_stm32f303.rs
@@ -1,12 +1,14 @@
 // 1. Define the vector table for the mcu
 
 // 2. Define the reset handler
+#[unsafe(no_mangle)]
 fn reset_handler() -> ! {
     // 1. Copy the .data section from FLASH to RAM
 
     // 2. Zero out the .bss section in the RAM
 
     // 3. Call main()
+    crate::main();
 }
 
 // 3. Define the exception handlers
