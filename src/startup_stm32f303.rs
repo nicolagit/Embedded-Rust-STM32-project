16b9f1425095610020fbe4280a18fe24c04a89c0
Nicola <nyc.zyp@gmail.com>
2025-09-06 22:53:06 +0100

    5.55 - Writing start-up code part-3 [extern "C"]

--- src/startup_stm32f303.rs
+++ src/startup_stm32f303.rs
@@ -1,4 +1,71 @@
 // 1. Define the vector table for the mcu
+pub static VECTOR_TABLE: [Option<unsafe fn()>; 48] = [
+    Some(reset_handler),
+    Some(nmi_handler),
+    Some(hardfault_handler),
+    Some(mem_manage_handler),
+    Some(busfault_handler),
+    Some(usagefault_handler),
+    None,
+    None,
+    None,
+    None, // Reserved
+    None, // SVCall
+    None, // DebugMonitor
+    None, // Reserved
+    None, // PendSV
+    None, // SysTick
+    // External Interrupts (IRQ0 - IRQ32 for STM32F303K8)
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None, // 15-24
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None, // 25-34
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None, // 35-44
+    None,
+    None,
+    None,
+    None, // 45-48
+];
+
+#[unsafe(no_mangle)]
+fn nmi_handler() {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+fn hardfault_handler() {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+fn default_handler() {
+    loop {}
+}
 
 // 2. Define the reset handler
 #[unsafe(no_mangle)]
