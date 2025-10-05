ae6811603ebdbbcbc230a9bca022d4b27e9acf75
Nicola <nyc.zyp@gmail.com>
2025-10-05 16:34:30 +0100

    5.60 - Writing start-up code part-6 (Reset_Handler)

--- src/startup_stm32f303.rs
+++ src/startup_stm32f303.rs
@@ -1,3 +1,6 @@
+
+use core::ptr;
+
 // 1. Define the vector table for the mcu
 unsafe extern "C" {
     fn BusFault_Handler();
@@ -7,13 +10,13 @@ unsafe extern "C" {
     fn SysTick_Handler();
     fn UsageFault_Handler();
     fn ADC1_2_Handler();
-    fn ADC3_Handler();
-    fn ADC4_Handler();
+    //fn ADC3_Handler();
+    //fn ADC4_Handler();
     fn CAN_RX1_Handler();
     fn CAN_SCE_Handler();
     fn COMP123_Handler();
     fn COMP456_Handler();
-    fn COMP7_Handler();
+    //fn COMP7_Handler();
     fn DMA1_CH1_Handler();
     fn DMA1_CH2_Handler();
     fn DMA1_CH3_Handler();
@@ -21,11 +24,11 @@ unsafe extern "C" {
     fn DMA1_CH5_Handler();
     fn DMA1_CH6_Handler();
     fn DMA1_CH7_Handler();
-    fn DMA2_CH1_Handler();
-    fn DMA2_CH2_Handler();
-    fn DMA2_CH3_Handler();
-    fn DMA2_CH4_Handler();
-    fn DMA2_CH5_Handler();
+    //fn DMA2_CH1_Handler();
+    //fn DMA2_CH2_Handler();
+    //fn DMA2_CH3_Handler();
+    //fn DMA2_CH4_Handler();
+    //fn DMA2_CH5_Handler();
     fn EXTI0_Handler();
     fn EXTI15_10_Handler();
     fn EXTI1_Handler();
@@ -34,60 +37,52 @@ unsafe extern "C" {
     fn EXTI4_Handler();
     fn EXTI9_5_Handler();
     fn FLASH_Handler();
-    fn FMC_Handler();
+    //fn FMC_Handler();
     fn I2C1_ER_Handler();
     fn I2C1_EV_EXTI23_Handler();
-    fn I2C2_ER_Handler();
-    fn I2C2_EV_EXTI24_Handler();
-    fn I2C3_ER_Handler();
-    fn I2C3_EV_Handler();
+    //fn I2C2_ER_Handler();
+    //fn I2C2_EV_EXTI24_Handler();
+    //fn I2C3_ER_Handler();
+    //fn I2C3_EV_Handler();
     fn PVD_Handler();
     fn RCC_Handler();
     fn RTCAlarm_Handler();
     fn RTC_WKUP_Handler();
     fn SPI1_Handler();
-    fn SPI2_Handler();
-    fn SPI3_Handler();
-    fn SPI4_Handler();
+    //fn SPI2_Handler();
+    //fn SPI3_Handler();
+    //fn SPI4_Handler();
     fn TAMP_STAMP_Handler();
     fn TIM1_BRK_TIM15_Handler();
     fn TIM1_CC_Handler();
     fn TIM1_TRG_COM_TIM17_Handler();
     fn TIM1_UP_TIM16_Handler();
-    fn TIM20_BRK_Handler();
-    fn TIM20_CC_Handler();
-    fn TIM20_TRG_COM_Handler();
-    fn TIM20_UP_Handler();
+    //fn TIM20_BRK_Handler();
+    //fn TIM20_CC_Handler();
+    //fn TIM20_TRG_COM_Handler();
+    //fn TIM20_UP_Handler();
     fn TIM2_Handler();
-    fn TIM3_Handler();
-    fn TIM4_Handler();
+    //fn TIM3_Handler();
+    //fn TIM4_Handler();
     fn TIM6_DACUNDER_Handler();
     fn TIM7_Handler();
-    fn TIM8_BRK_Handler();
-    fn TIM8_CC_Handler();
-    fn TIM8_TRG_COM_Handler();
-    fn TIM8_UP_Handler();
-    fn UART4_EXTI34_Handler();
-    fn UART5_EXTI35_Handler();
+    //fn TIM8_BRK_Handler();
+    //fn TIM8_CC_Handler();
+    //fn TIM8_TRG_COM_Handler();
+    //fn TIM8_UP_Handler();
+    //fn UART4_EXTI34_Handler();
+    //fn UART5_EXTI35_Handler();
     fn USART1_EXTI25_Handler();
     fn USART2_EXTI26_Handler();
     fn USART3_EXTI28_Handler();
     fn USB_HP_CAN_TX_Handler();
-    fn USB_HP_Handler();
+    //fn USB_HP_Handler();
     fn USB_LP_CAN_RX0_Handler();
-    fn USB_LP_Handler();
-    fn USB_WKUP_EXTI_Handler();
-    fn USB_WKUP_Handler();
+    //fn USB_LP_Handler();
+    //fn USB_WKUP_EXTI_Handler();
+    //fn USB_WKUP_Handler();
     fn WWDG_Handler();
     fn FPU_Handler();
-}
-
-unsafe extern "C" {
-    static _sidata: u32; // Start of .data section in flash
-    static _sdata: u32; // Start of .data section in RAM
-    static _edata: u32; // End of .data section in RAM
-    static _sbss: u32; // Start of .bss section in RAM
-    static _ebss: u32; // End of .bss section in RAM
 }
 
 #[unsafe(link_section = ".isr_vector")] //#[link_section = ".isr_vector"]
@@ -209,17 +204,41 @@ extern "C" fn Default_Handler() {
     loop {}
 }
 
+unsafe extern "C" {
+    static _sidata: u32; // Start of .data section in flash
+    static mut _sdata: u32; // Start of .data section in RAM
+    static mut _edata: u32; // End of .data section in RAM
+    static mut _sbss: u32; // Start of .bss section in RAM
+    static mut _ebss: u32; // End of .bss section in RAM
+}
+
 // 2. Define the reset handler
 #[unsafe(no_mangle)] //#[no_mangle]
 unsafe extern "C" fn Reset_Handler() {
+    unsafe {
     // 1. Copy the .data section from FLASH to RAM
 
-    // reference of static variable to C like raw pointer.
-    let src_is_flash: = &_sidata as *const u32;
+        // reference of static variable to C like raw pointer.
+        let mut src_is_flash = ptr::addr_of!(_sidata);
+        let mut dest_is_ram = ptr::addr_of_mut!( _sdata);
+        let data_end_in_ram = ptr::addr_of_mut!( _edata);
+
+        while dest_is_ram < data_end_in_ram {
+            *dest_is_ram = *src_is_flash;
+            dest_is_ram = dest_is_ram.add(1);
+            src_is_flash = src_is_flash.add(1);
+        }
 
     // 2. Zero out the .bss section in the RAM
+        let mut bss = ptr::addr_of_mut!(_sbss);
+        let bss_end = ptr::addr_of_mut!(_ebss);
+        while bss < bss_end {
+            *bss = 0;
+            bss = bss.add(1);
+        }
+    }
 
-    // 3. Call main()
+// 3. Call main()
     crate::main();
 }
 
